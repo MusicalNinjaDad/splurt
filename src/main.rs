693c5b0051a541d6621d68e79a08782abec9e50f
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-17 14:21:43 +0000

    skeleton for list command

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,45 @@
-fn main() {}
+#![feature(never_type)]
+#![feature(try_trait_v2)]
+#![feature(try_trait_v2_residual)]
+
+use std::{fmt::Debug, process::Termination as _T};
+
+use clap::{Parser, Subcommand};
+use exit_safely::Termination;
+use try_v2::{Try, Try_ConvertResult};
+
+#[derive(Parser)]
+#[command(version)]
+struct Splurt {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    /// list interfaces
+    List,
+}
+
+fn main() -> Exit<()> {
+    let splurt = Splurt::try_parse()?;
+
+    match &splurt.command {
+        Command::List => todo!(),
+    }
+}
+
+#[derive(Debug, Termination, Try, Try_ConvertResult, PartialEq, PartialOrd, Eq, Ord)]
+#[repr(u8)]
+#[must_use]
+pub enum Exit<T: _T> {
+    Ok(T) = 0,
+    Error(String) = 1,
+    InvocationError(String) = 2,
+}
+
+impl<T: _T> From<clap::Error> for Exit<T> {
+    fn from(e: clap::Error) -> Self {
+        Self::InvocationError(e.to_string())
+    }
+}
