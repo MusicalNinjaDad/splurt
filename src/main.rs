00d727403bb55a31521786f3fcf6743cc048fa70
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-17 14:29:16 +0000

    list network interfaces

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@
 #![feature(try_trait_v2)]
 #![feature(try_trait_v2_residual)]
 
-use std::{fmt::Debug, process::Termination as _T};
+use std::{fmt::Debug, io, process::Termination as _T};
 
 use clap::{Parser, Subcommand};
+use cotton_netif::get_interfaces;
 use exit_safely::Termination;
 use try_v2::{Try, Try_ConvertResult};
 
@@ -25,8 +26,13 @@ fn main() -> Exit<()> {
     let splurt = Splurt::try_parse()?;
 
     match &splurt.command {
-        Command::List => todo!(),
+        Command::List => {
+            for e in get_interfaces()? {
+                println!("{:?}", e);
+            }
+        }
     }
+    Exit::Ok(())
 }
 
 #[derive(Debug, Termination, Try, Try_ConvertResult, PartialEq, PartialOrd, Eq, Ord)]
@@ -36,10 +42,17 @@ pub enum Exit<T: _T> {
     Ok(T) = 0,
     Error(String) = 1,
     InvocationError(String) = 2,
+    IO(String) = 3,
 }
 
 impl<T: _T> From<clap::Error> for Exit<T> {
     fn from(e: clap::Error) -> Self {
         Self::InvocationError(e.to_string())
+    }
+}
+
+impl<T: _T> From<io::Error> for Exit<T> {
+    fn from(e: io::Error) -> Self {
+        Self::IO(e.to_string())
     }
 }
