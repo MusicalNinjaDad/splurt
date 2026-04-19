6e6ac0a9c76a1828099b95cf26d90cade7ff033b
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-19 17:19:23 +0000

    handle byebye

--- src/main.rs
+++ src/main.rs
@@ -69,17 +69,32 @@ async fn main() -> Exit<()> {
                             }) => {
                                 match known_services.insert(unique_service_name.clone(), KnownService::from_notification(notification).expect("This is an alive")) {
                                     None => {
-                                        println!("+ {notification_type}");
-                                        println!("  {unique_service_name} at {location}");
+                                        println!("+  {notification_type}");
+                                        println!("   {unique_service_name} at {location}");
                                     }
                                     Some(previous) if previous != KnownService::from_notification(notification).expect("This is an alive") => {
-                                        println!("! {} -> {}", previous.service_type, notification_type);
-                                        println!("  {unique_service_name} at {} -> {}", previous.location, location);
+                                        println!("!  {} -> {}", previous.service_type, notification_type);
+                                        println!("   {unique_service_name} at {} -> {}", previous.location, location);
                                     },
                                     Some(_) => (),
                                 }
                             }
-                            Some(Notification::ByeBye{..}) => todo!(),
+                            Some(Notification::ByeBye{ notification_type, unique_service_name }) => {
+                                match known_services.remove_entry(&unique_service_name) {
+                                    None =>{
+                                        println!("+- {notification_type}");
+                                        println!("   {unique_service_name} at unknown");
+                                    },
+                                    Some((_, previous)) => {
+                                        if previous.service_type == notification_type {
+                                            println!("!- {} -> {}", previous.service_type, notification_type);
+                                        } else {
+                                            println!(" - {}", previous.service_type);
+                                        }
+                                        println!("   {unique_service_name} at {}", previous.location);
+                                    },
+                                }
+                            }
                             None => todo!(),
                         }
                     },
