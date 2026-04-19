bf814bc262a3518034f0cb30940e7c21d71ab009
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-19 15:02:35 +0000

    add todo note

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,8 @@ async fn main() -> Exit<()> {
                 println!("{:?}", e);
             }
         }
+        //TODO: This is directly from the cotton example and needs a bit of rework.
+        //      See #5 & #6
         Command::Ssdp => {
             let mut netif = cotton_netif::get_interfaces_async()?;
             let mut known_services = HashMap::<String, Notification>::new();
