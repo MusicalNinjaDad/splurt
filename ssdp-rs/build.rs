966ecc2ce227517c7447759c4de89bbdb1cf2a8b
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-20 06:34:47 +0000

    skeleton parse alive Message

---
+++ ssdp-rs/build.rs
@@ -0,0 +1,137 @@
+use std::{env::VarError, ffi::OsString, fmt::Display};
+
+use autocfg::AutoCfg;
+
+fn main() -> Result<(), BuildError> {
+    let ac = autocfg::new();
+    ac.emit_unstable_feature("assert_matches");
+    AssertMatchesLocation::emit_possibilities();
+    if let Some(location) = ac.assert_matches_location() {
+        autocfg::emit(&location.to_string())
+    }
+
+    Ok(())
+}
+
+#[allow(unused, reason = "not using clap here")]
+fn get_var(key: &str) -> Result<String, BuildError> {
+    std::env::var(key).map_err(|e| BuildError::from_var_error(key, e))
+}
+
+#[derive(Debug)]
+#[expect(
+    unused,
+    reason = "error contents output to stderr in Termination for Result"
+)]
+enum BuildError {
+    VarNotSet(OsString),
+    VarInvalid(OsString, OsString),
+    IOError(Box<std::io::Error>),
+}
+
+impl BuildError {
+    fn from_var_error(var: &str, e: VarError) -> BuildError {
+        match e {
+            VarError::NotPresent => BuildError::VarNotSet(var.into()),
+            VarError::NotUnicode(contents) => BuildError::VarInvalid(var.into(), contents),
+        }
+    }
+}
+
+impl From<std::io::Error> for BuildError {
+    fn from(e: std::io::Error) -> Self {
+        BuildError::IOError(Box::new(e))
+    }
+}
+
+/// Location of assert_matches!() macro. Stabilisation was reverted at last minute
+/// on 2026-04-10, leaving the macro in the new planned location.
+enum AssertMatchesLocation {
+    /// Macro is at `std::assert_matches`
+    Root,
+    /// Macro is at `std::assert_matches::assert_matches`
+    Module,
+}
+
+impl Display for AssertMatchesLocation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            AssertMatchesLocation::Root => write!(f, "assert_matches_in_root"),
+            AssertMatchesLocation::Module => write!(f, "assert_matches_in_module"),
+        }
+    }
+}
+
+impl AssertMatchesLocation {
+    fn emit_possibilities() {
+        autocfg::emit_possibility(&AssertMatchesLocation::Root.to_string());
+        autocfg::emit_possibility(&AssertMatchesLocation::Module.to_string());
+    }
+}
+
+trait Nightly {
+    /// Identify whether a an experimental feature flag is available _and_ required on nightly.
+    /// Always fails if feature flags are unavailable.
+    ///
+    /// ## Usage:
+    /// To be used at top-level crate via `#![cfg_attr(unstable_foo, feature(foo))]`
+    fn emit_unstable_feature(&self, feature: &'static str);
+
+    /// Location of assert_matches!() macro. Stabilisation was reverted at last minute
+    /// on 2026-04-10, leaving the macro in the new planned location.
+    ///
+    /// #Recommended usage
+    /// ```
+    /// AssertMatchesLocation::emit_possibilities();
+    /// if let Some(location) = ac.assert_matches_location() {
+    ///     autocfg::emit(&location.to_string())
+    /// }
+    /// ```
+    fn assert_matches_location(&self) -> Option<AssertMatchesLocation>;
+}
+
+impl Nightly for AutoCfg {
+    fn emit_unstable_feature(&self, feature: &'static str) {
+        let cfg = format!("unstable_{feature}");
+        let code = format!(
+            r#"
+        #![deny(stable_features)]
+        #![feature({feature})]
+        "#
+        );
+        autocfg::emit_possibility(&cfg);
+        if self.probe_raw(&code).is_ok() {
+            autocfg::emit(&cfg);
+        }
+    }
+
+    fn assert_matches_location(&self) -> Option<AssertMatchesLocation> {
+        let in_root = r#"
+        #![allow(stable_features)]
+        #![feature(assert_matches)]
+        use std::assert_matches;
+
+        fn main() {
+            assert_matches!(Some(4), Some(_));
+        }
+            "#;
+
+        let in_module = r#"
+        #![allow(stable_features)]
+        #![feature(assert_matches)]
+        use std::assert_matches::assert_matches;
+
+        fn main() {
+            assert_matches!(Some(4), Some(_));
+        }
+            "#;
+
+        if self.probe_raw(in_root).is_ok() {
+            Some(AssertMatchesLocation::Root)
+        } else if self.probe_raw(in_module).is_ok() {
+            Some(AssertMatchesLocation::Module)
+        } else {
+            None
+        }
+    }
+}
--- ssdp-rs/src/lib.rs
+++ ssdp-rs/src/lib.rs
@@ -1,1 +1,52 @@
+#![cfg_attr(unstable_assert_matches, feature(assert_matches))]
 
+/// A valid & parsed ssdp message
+///
+/// Create with `Message::parse()`
+#[derive(Debug, Clone, PartialEq, PartialOrd)]
+pub enum Message {
+    /// NTS: ssdp:alive
+    Alive,
+}
+
+impl Message {
+    pub fn parse(_contents: &str) -> Option<Message> {
+        Some(Message::Alive)
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[cfg(assert_matches_in_root)]
+    use std::assert_matches;
+
+    #[cfg(assert_matches_in_module)]
+    use std::assert_matches::assert_matches;
+
+    const ALIVE: &str = r#"NOTIFY * HTTP/1.1
+Host: 239.255.255.250:1982
+Cache-Control: max-age=3600
+Location: yeelight://192.168.1.239:55443
+NTS: ssdp:alive
+Server: POSIX, UPnP/1.0 YGLC/1
+id: 0x000000000015243f
+model: color
+fw_ver: 18
+support: get_prop set_default set_power toggle set_bright start_cf stop_cf set_scene cron_add cron_get cron_del set_ct_abx set_rgb
+power: on
+bright: 100
+color_mode: 2
+ct: 4000
+rgb: 16711680
+hue: 100
+sat: 35
+name: my_bulb
+"#;
+
+    #[test]
+    fn parse_alive() {
+        let msg = Message::parse(ALIVE).unwrap();
+        assert_matches!(msg, Message::Alive);
+    }
+}
