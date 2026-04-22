1d796de51e6d82430574359841f3275f70aab7b0
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-22 06:20:13 +0000

    let OS select port & tidy with comments & whitespace

--- ssdp-rs/tests/loopback.rs
+++ ssdp-rs/tests/loopback.rs
@@ -6,18 +6,29 @@ use futures_net::{TcpListener, TcpStream, runtime::Runtime};
 #[futures_net::test]
 async fn tcp() {
     let loopback = Ipv4Addr::new(127, 0, 0, 1);
-    let addr: SocketAddr = SocketAddrV4::new(loopback, 9999).into();
+    
+    // https://doc.rust-lang.org/std/net/struct.TcpListener.html#method.bind
+    // Binding with a port number of 0 will request that the OS assigns a port to this listener.
+    // The port allocated can be queried via the TcpListener::local_addr method.
+    let addr: SocketAddr = SocketAddrV4::new(loopback, 0).into();
     let mut receiver = TcpListener::bind(&addr).expect("receiver");
+    let addr = receiver.local_addr().expect("bound port");
+    dbg!(addr);
+
     let mut sender = TcpStream::connect(&addr).await.expect("sender");
+    
     let mut received: [u8; 17] = [b'\x00'; 17];
     let msg: &[u8; 17] = b"tcp loopback test";
+    
     let send = async move {
         println!("sending {}", String::from_utf8_lossy(msg));
         sender.write_all(msg).await.expect("send msg");
         println!("closing sender");
         sender.close().await.expect("closing sender");
     };
+    
     let rec = async {
+        println!("initiating receiver");
         if let Some(stream) = receiver.incoming().next().await {
             println!("receiving ...");
             stream
@@ -28,9 +39,10 @@ async fn tcp() {
             println!("received: {}", String::from_utf8_lossy(&received));
         };
     };
+
     println!("ready to join");
     futures::join!(rec, send);
-    dbg!(&received);
+    
     assert_eq!(
         String::from_utf8_lossy(&received),
         String::from_utf8_lossy(msg)
