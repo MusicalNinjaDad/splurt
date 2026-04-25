9d9503aaa68991eea0e91917ff48663dd3f01ce9
MusicalNinjaDad <102677655+MusicalNinjaDad@users.noreply.github.com>
2026-04-25 09:35:19 +0200

    Xtask build (#18)
    build, optionally with zigbuild for a given glibc version

--- xtask/src/commands.rs
+++ xtask/src/commands.rs
@@ -52,3 +52,29 @@ pub fn test(root: &Path) -> Spawned {
         .spawn()
         .into_spawned("tests")
 }
+
+/// Spawn `cargo build` (if no `glibc` specified) / `cargo zigbuild` (if `glibc` specified)
+/// optionally performing a release build (default is cargo's default profile)
+///
+/// #### Build target (TODO: take a specific target & other cargo args)
+/// - For a given `glibc`: `x86_64-unknown-linux-gnu`
+/// - Otherwise: cargo default target
+pub fn build(root: &Path, glibc: &Option<String>, release: &bool) -> Spawned {
+    let (builder, target) = match glibc {
+        Some(version) => {
+            let target = format!("x86_64-unknown-linux-gnu.{}", version);
+            ("zigbuild", vec!["--target".to_string(), target])
+        }
+        None => ("build", vec![]),
+    };
+    let release = if *release { Some("--release") } else { None };
+    Command::new("cargo")
+        .current_dir(root)
+        .arg(builder)
+        .args(release)
+        .args(target)
+        .stderr(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .into_spawned("build")
+}
