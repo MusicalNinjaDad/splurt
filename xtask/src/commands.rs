c111ad1765c27e6d0bd72d541fa751aafaca619d
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-25 07:14:49 +0000

    add build command

--- xtask/src/commands.rs
+++ xtask/src/commands.rs
@@ -3,6 +3,7 @@ use std::{
     process::{Command, Stdio},
 };
 
+
 use crate::{Cmd, CmdExt as _, Spawned, SpawnedExt as _};
 
 pub fn fmt(root: &Path) -> Cmd {
@@ -51,4 +52,30 @@ pub fn test(root: &Path) -> Spawned {
         .stdout(Stdio::piped())
         .spawn()
         .into_spawned("tests")
+}
+
+/// Spawn `cargo build` (if no `glibc` specified) / `cargo zigbuild` (if `glibc` specified)
+/// optionally performing a release build (default is cargo's default profile)
+/// 
+/// #### Build target (TODO: take a specific target & other cargo args)
+/// - For a given `glicb`: `x86_64-unknown-linux-gnu`
+/// - Otherwise: cargo default target 
+pub fn build(root: &Path, glibc: &Option<String>, release: &bool) -> Spawned {
+    let (builder, target) = match glibc {
+        Some(version) => {
+            let target = format!("x86_64-unknown-linux-gnu.{}", version);
+            ("zigbuild", vec!["--target".to_string(), target])
+        }
+        None => ("build", vec![]),
+    };
+    let release = if *release { Some("--release") } else { None };
+    Command::new("cargo")
+        .current_dir(root)
+        .arg(builder)
+        .args(release)
+        .args(target)
+        .stderr(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .into_spawned("build")
 }
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -3,7 +3,7 @@ use std::path::Path;
 use clap::{Parser, Subcommand};
 use try_v2_xtasks::{
     Exit,
-    commands::{clippy, clippy_tests, fmt, git_add, test},
+    commands::{build, clippy, clippy_tests, fmt, git_add, test},
 };
 
 #[derive(Parser)]
@@ -17,14 +17,23 @@ struct XTask {
 enum Command {
     /// git add if all is good
     Add,
+    /// build (optionally with zigbuild for a given glibc version)
+    Build {
+        /// build for a specific glibc version (WSL-Ubuntu is 2.35)
+        #[arg(short, long)]
+        glibc: Option<String>,
+        /// build a release build (default is cargo's default profile, usually debug)
+        #[arg(short, long)]
+        release: bool,
+    },
 }
 
 fn main() -> Exit<()> {
     let xtask = XTask::try_parse()?;
+    let root = Path::new(".");
 
     match &xtask.command {
         Command::Add => {
-            let root = Path::new(".");
             let fmt = fmt(root);
             Exit::from(fmt)?;
             let clippy = clippy(root);
@@ -34,6 +43,10 @@ fn main() -> Exit<()> {
             Exit::from(checks)?;
             let git = git_add(root);
             Exit::from(git)
+        }
+        Command::Build { glibc, release } => {
+            let build = build(root, glibc, release);
+            Exit::from(vec![build])
         }
     }
 }
