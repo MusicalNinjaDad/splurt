bd68384f2e3bc8d58c5cec5cfaeee69ed0b73ea8
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-25 15:10:38 +0000

    remove default field values

--- ssdp-rs/src/lib.rs
+++ ssdp-rs/src/lib.rs
@@ -1,6 +1,5 @@
 #![cfg_attr(all(test, unstable_assert_matches), feature(assert_matches))]
 #![cfg_attr(unstable_bool_to_result, feature(bool_to_result))]
-#![cfg_attr(unstable_default_field_values, feature(default_field_values))]
 #![cfg_attr(unstable_let_chains, feature(let_chains))]
 
 pub mod message;
