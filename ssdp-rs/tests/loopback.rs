f278ab6ae97142e393d27a7440929b6dd33bcab6
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-27 10:11:35 +0000

    remove UdpConnectedStream

--- ssdp-rs/tests/loopback.rs
+++ ssdp-rs/tests/loopback.rs
@@ -5,7 +5,7 @@ use std::{
 
 use futures::prelude::*;
 use futures_net::{TcpListener, TcpStream, runtime::Runtime};
-use ssdp_rs::udp::{EventedUdpSocket, UdpConnectedStream, UdpSink, UdpStream};
+use ssdp_rs::udp::{EventedUdpSocket, UdpSink, UdpStream};
 
 #[futures_net::test]
 async fn tcp() {
@@ -42,116 +42,6 @@ async fn tcp() {
                 .expect("received something");
             println!("received: {}", String::from_utf8_lossy(&received));
         };
-    };
-
-    println!("ready to join");
-    futures::join!(rec, send);
-
-    assert_eq!(
-        String::from_utf8_lossy(&received),
-        String::from_utf8_lossy(msg)
-    );
-}
-
-#[futures_net::test]
-async fn udp() {
-    let loopback = Ipv4Addr::new(127, 0, 0, 1);
-
-    // https://doc.rust-lang.org/std/net/struct.TcpListener.html#method.bind
-    // Binding with a port number of 0 will request that the OS assigns a port to this listener.
-    // The port allocated can be queried via the TcpListener::local_addr method.
-    let addr: SocketAddr = SocketAddrV4::new(loopback, 0).into();
-
-    let mut receiver = UdpStream::<32>::bind(addr).expect("receiver");
-    let rec_addr = receiver.local_addr().expect("bound port");
-    dbg!(rec_addr);
-
-    let mut sender = UdpConnectedStream::new(&rec_addr).expect("sender");
-    let send_addr = sender.local_addr().expect("bound port");
-    dbg!(send_addr);
-    let connected_addr = sender.connected_to().expect("connected");
-    assert_eq!(connected_addr, rec_addr);
-
-    let mut received: [u8; 17] = [b'\x00'; 17];
-    let msg: &[u8; 17] = b"udp loopback test";
-
-    let send = async move {
-        println!("sending {}", String::from_utf8_lossy(msg));
-        sender.write_all(msg).await.expect("send msg");
-        println!("flushing sender");
-        sender.flush().await.expect("flushed sender");
-        println!("closing sender");
-        sender.close().await.expect("closing sender");
-    };
-
-    let rec = async {
-        println!("initiating receiver");
-        let (bytes, sent_by) = receiver
-            .recv_from(&mut received)
-            .await
-            .expect("valid message");
-        println!(
-            "received: {} from {} ({} bytes)",
-            String::from_utf8_lossy(&received),
-            sent_by,
-            bytes
-        );
-    };
-
-    println!("ready to join");
-    futures::join!(rec, send);
-
-    assert_eq!(
-        String::from_utf8_lossy(&received),
-        String::from_utf8_lossy(msg)
-    );
-}
-
-#[futures_net::test]
-async fn next_to_read() {
-    let loopback = Ipv4Addr::new(127, 0, 0, 1);
-
-    // https://doc.rust-lang.org/std/net/struct.TcpListener.html#method.bind
-    // Binding with a port number of 0 will request that the OS assigns a port to this listener.
-    // The port allocated can be queried via the TcpListener::local_addr method.
-    let addr: SocketAddr = SocketAddrV4::new(loopback, 0).into();
-
-    let mut receiver = UdpStream::<32>::bind(addr).expect("receiver");
-    let rec_addr = receiver.local_addr().expect("bound port");
-    dbg!(rec_addr);
-
-    let mut sender = UdpConnectedStream::new(&rec_addr).expect("sender");
-    let send_addr = sender.local_addr().expect("bound port");
-    dbg!(send_addr);
-    let connected_addr = sender.connected_to().expect("connected");
-    assert_eq!(connected_addr, rec_addr);
-
-    let mut received: [u8; 17] = [b'\x00'; 17];
-    let msg: &[u8; 17] = b"udp loopback test";
-
-    let send = async move {
-        println!("sending {}", String::from_utf8_lossy(msg));
-        sender.write_all(msg).await.expect("send msg");
-        println!("flushing sender");
-        sender.flush().await.expect("flushed sender");
-        println!("closing sender");
-        sender.close().await.expect("closing sender");
-    };
-
-    let rec = async {
-        println!("initiating receiver");
-        let (msg, len, sent_by) = receiver
-            .next()
-            .await
-            .expect("a message")
-            .expect("a valid message");
-        println!(
-            "received: {} from {} ({} bytes)",
-            String::from_utf8_lossy(&msg),
-            sent_by,
-            len
-        );
-        received = msg[..len].try_into().expect("17 bytes in msg");
     };
 
     println!("ready to join");
