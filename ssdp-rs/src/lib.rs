dbf574faa12ae3f65cd8b2ddd2ba93fc5b227457
Mike Foster <i.am.mike.foster@gmail.com>
2026-04-29 15:19:40 +0000

    tidy experimental features

--- ssdp-rs/src/lib.rs
+++ ssdp-rs/src/lib.rs
@@ -1,7 +1,8 @@
 #![cfg_attr(all(test, unstable_assert_matches), feature(assert_matches))]
+#![cfg_attr(unstable_adt_const_params, feature(adt_const_params))]
 #![cfg_attr(unstable_let_chains, feature(let_chains))]
 #![cfg_attr(unstable_never_type, feature(never_type))]
-#![feature(adt_const_params)]
+#![allow(incomplete_features)]
 #![feature(unsized_const_params)]
 
 //! A runtime-agnostic (known to work with tokio, futures-rs and futures-net executors) async ssdp
