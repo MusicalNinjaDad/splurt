96431d922a6457a07e07d477bd4c593ef4b03f22
MusicalNinjaDad <102677655+MusicalNinjaDad@users.noreply.github.com>
2026-04-29 07:18:03 +0200

    type-safely generate and parse M-SEARCH (#38)
    - adds Header & HeaderExt traits
    - adds Display & FromStr impls
    - adds dedicated types for Header fields as per spec

--- ssdp-rs/src/message.rs
+++ ssdp-rs/src/message.rs
@@ -7,9 +7,75 @@
 //!
 //! [spec]: https://openconnectivity.org/upnp-specs/UPnP-arch-DeviceArchitecture-v2.0-20200417.pdf
 
-use std::{collections::HashMap, fmt::Display, io};
+use std::{
+    collections::HashMap,
+    error,
+    fmt::Display,
+    io,
+    net::{AddrParseError, SocketAddr, SocketAddrV4, SocketAddrV6},
+    str::FromStr,
+};
 
 use uuid::Uuid;
+
+use crate::MULTICAST;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub enum ParseError {
+    InvalidMethod(String),
+    InvalidST(String),
+    InvalidDevice(String),
+    InvalidDeviceDetails(String),
+}
+
+impl error::Error for ParseError {}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParseError::InvalidMethod(method) => write!(f, "{} is not a valid upnp method", method),
+            ParseError::InvalidST(st) => write!(f, "{} is not a valid upnp search type", st),
+            ParseError::InvalidDevice(device) => write!(
+                f,
+                "{} is not a valid upnp device specification (valid forms are `urn:domain-name:device:deviceType:ver` & `urn:schemas-upnp-org:device:deviceType:ver`)",
+                device
+            ),
+            ParseError::InvalidDeviceDetails(device) => {
+                write!(f, "{} is not a valid upnp device:ver specification", device)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Method {
+    MSearch,
+    Notify,
+    Response,
+}
+
+impl Display for Method {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Method::MSearch => write!(f, "M-SEARCH * HTTP/1.1"),
+            Method::Notify => write!(f, "NOTIFY * HTTP/1.1"),
+            Method::Response => write!(f, "HTTP/1.1 200 OK"),
+        }
+    }
+}
+
+impl FromStr for Method {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "M-SEARCH * HTTP/1.1" => Ok(Self::MSearch),
+            "NOTIFY * HTTP/1.1" => Ok(Self::Notify),
+            "HTTP/1.1 200 OK" => Ok(Self::Response),
+            _ => Err(ParseError::InvalidMethod(s.to_string())),
+        }
+    }
+}
 
 /// A valid & parsed ssdp message
 ///
@@ -20,112 +86,6 @@ pub enum Message {
     Alive(Notification),
     /// MAN: ssdp:discover
     Search(MSearch),
-}
-
-/// Formats Message as per OCF specification (2020)
-impl Display for Message {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Message::Alive(_notification) => todo!("display alive messages"),
-            Message::Search(msearch) => {
-                write!(f, "{msearch}")
-            }
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-pub struct MSearch {
-    mx: Mx,
-    user_agent: Option<UserAgent>,
-    friendly_name: String,
-    uuid: Option<Uuid>,
-}
-
-#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
-/// A valid MX value (0..=5) (see UPnP spec para 1.3.2)
-///
-/// - Construct via `TryFrom<u8>`
-/// - Desconstruct via `Into<u8>`
-/// - Invalid values will result in an `io::ErrorKind::InvalidInput`.
-pub struct Mx(u8);
-
-impl Display for Mx {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{}", self.0)
-    }
-}
-
-impl TryFrom<u8> for Mx {
-    type Error = io::Error;
-
-    fn try_from(mx: u8) -> Result<Self, Self::Error> {
-        match mx {
-            0..=5 => Ok(Self(mx)),
-            _ => Err(io::Error::from(io::ErrorKind::InvalidInput)),
-        }
-    }
-}
-
-impl From<Mx> for u8 {
-    fn from(mx: Mx) -> Self {
-        mx.0
-    }
-}
-
-/// Entire valid M-SEARCH message including initial method line,
-/// as per OCF specification (2020) section 1.3.2
-///
-/// #### Note:
-/// I've rarely actually seen a well-formed spec-conform M-SEARCH flying around my network
-/// but there's nothing wrong with actually being fully valid!
-impl Display for MSearch {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self {
-            mx,
-            user_agent,
-            friendly_name,
-            uuid,
-        } = self;
-        writeln!(f, "M-SEARCH * HTTP/1.1")?;
-        writeln!(f, "HOST: 239.255.255.250:1900")?;
-        writeln!(f, r#"MAN: "ssdp:discover""#)?;
-        writeln!(f, "MX: {}", mx)?;
-        writeln!(f, "ST: ssdp:all")?;
-        if let Some(user_agent) = user_agent {
-            writeln!(f, "USER-AGENT: {}", user_agent)?;
-        }
-        writeln!(f, "CPFN.UPNP.ORG: {}", friendly_name)?;
-        if let Some(uuid) = uuid {
-            writeln!(f, "CPUUID.UPNP.ORG: {}", uuid)?;
-        }
-        writeln!(f)
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
-struct UserAgent {
-    os: String,
-    os_version: String,
-    product_name: String,
-    product_version: String,
-}
-
-/// Formatted as per OCF specification (2020) section 1.3.2 for the `USER-AGENT` *value*,
-/// does NOT include the header key
-impl Display for UserAgent {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let Self {
-            os,
-            os_version,
-            product_name,
-            product_version,
-        } = self;
-        write!(
-            f,
-            "{os}/{os_version} UPnP/2.0 {product_name}/{product_version}"
-        )
-    }
 }
 
 impl Message {
@@ -168,15 +128,441 @@ impl Message {
             product_name: product_name.to_string(),
             product_version: product_version.to_string(),
         };
+        let host = Default::default();
         Message::Search(MSearch {
+            host,
             mx,
             user_agent: Some(user_agent),
-            friendly_name: friendly_name.to_string(),
+            friendly_name: friendly_name.into(),
             uuid: Some(uuid),
         })
     }
 }
 
+/// Formats Message as per OCF specification (2020)
+impl Display for Message {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Message::Alive(_notification) => todo!("display alive messages"),
+            Message::Search(msearch) => {
+                write!(f, "{msearch}")
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Man {
+    Discover,
+}
+
+impl Display for Man {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let str = match self {
+            Man::Discover => "ssdp:discover",
+        };
+        // MAN values are enclosed in double-quotes
+        write!(f, r#""{}""#, str)
+    }
+}
+
+impl Header for Man {
+    const HEADER_KEY: &'static str = "MAN";
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+/// Search Target
+enum ST {
+    /// `ssdp:all`: Search for all devices and services.
+    All,
+    /// `upnp:rootdevice`: Search for root devices only.
+    #[expect(unused)]
+    Root,
+    /// uuid:device-UUID: Search for a particular device.
+    #[expect(unused)]
+    Uuid(Uuid),
+    /// `urn:schemas-upnp-org:device:deviceType:ver`:
+    ///     Search for any device of this type where `deviceType` and `ver` are
+    ///     defined by the UPnP Forum working committee.
+    /// `urn:domain-name:device:deviceType:ver`:
+    ///     Search for any device of this typewhere domain-name (a Vendor Domain Name),
+    ///     deviceType and ver are defined by the UPnP vendor and ver specifies the highest
+    ///     specifies the highest supported version of the device type. Period characters in
+    ///     the Vendor Domain Name shall be replaced with hyphens in accordance with RFC 2141.
+    /// TODO: #36 DeviceTypes
+    #[expect(unused)]
+    Device(DeviceDetails),
+    /// `urn:schemas-upnp-org:service:serviceType:ver`:
+    ///     Search for any service of this type where serviceType and ver are
+    ///     defined by the UPnP Forum working committee.
+    /// `urn:domain-name:service:serviceType:ver`:
+    ///     Search for any service of this type. Where domain-name (a Vendor Domain Name),
+    ///     serviceType and ver are defined by the UPnP vendor and ver specifies the highest
+    ///     specifies the highest supported version of the service type. Period characters in
+    ///     the Vendor Domain Name shall be replaced with hyphens in accordance with RFC 2141.
+    /// TODO: #37 ServiceTypes
+    #[expect(unused)]
+    Service(ServiceDetails),
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct DeviceDetails {
+    vendor: Vendor,
+    device: Device,
+}
+
+impl Display for DeviceDetails {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:device:{}", self.vendor, self.device)
+    }
+}
+
+impl FromStr for DeviceDetails {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let err = || ParseError::InvalidDevice(s.to_string());
+        let mut parts = s.split(":");
+        match parts.next() {
+            Some("urn") => (),
+            _ => return Err(err()),
+        };
+        let Ok(vendor) = parts.next().ok_or_else(err)?.parse::<Vendor>();
+        match parts.next() {
+            Some("device") => (),
+            _ => return Err(err()),
+        };
+        let device: String = parts.collect();
+        let device: Device = device.parse()?;
+        Ok(Self { vendor, device })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum Vendor {
+    Standard,
+    Custom(String),
+}
+
+impl Display for Vendor {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Vendor::Standard => write!(f, "schemas-upnp-org"),
+            Vendor::Custom(s) => write!(f, "{}", s),
+        }
+    }
+}
+
+impl FromStr for Vendor {
+    type Err = !;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "schemas-upnp-org" => Ok(Self::Standard),
+            _ => Ok(Self::Custom(s.to_string())),
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum Device {
+    Other { device_type: String, ver: String },
+}
+
+impl Display for Device {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Device::Other { device_type, ver } => write!(f, "{}:{}", device_type, ver),
+        }
+    }
+}
+
+impl FromStr for Device {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (device_type, ver) = s
+            .split_once(":")
+            .ok_or(ParseError::InvalidDeviceDetails(s.to_string()))?;
+        Ok(Self::Other {
+            device_type: device_type.to_string(),
+            ver: ver.to_string(),
+        })
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct ServiceDetails {
+    vendor: Vendor,
+    service: Service,
+}
+
+impl Display for ServiceDetails {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}:service:{}", self.vendor, self.service)
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+enum Service {
+    #[expect(unused)]
+    Other { service_type: String, ver: String },
+}
+
+impl Display for Service {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Service::Other { service_type, ver } => write!(f, "{}:{}", service_type, ver),
+        }
+    }
+}
+
+impl Header for ST {
+    const HEADER_KEY: &'static str = "ST";
+}
+
+impl Display for ST {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ST::All => write!(f, "ssdp:all"),
+            ST::Root => write!(f, "upnp:rootdevice"),
+            ST::Uuid(uuid) => write!(f, "uuid:device-{}", uuid),
+            ST::Device(device_details) => write!(f, "urn:{device_details}"),
+            ST::Service(service_details) => write!(f, "urn:{service_details}"),
+        }
+    }
+}
+
+//TODO: impl FromStr for ST {
+//     type Err = Error;
+//
+//     fn from_str(s: &str) -> Result<Self, Self::Err> {
+//         todo!("from str ST")
+//     }
+// }
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct MSearch {
+    host: Host,
+    mx: Mx,
+    user_agent: Option<UserAgent>,
+    friendly_name: FriendlyName,
+    uuid: Option<Uuid>,
+}
+
+/// Entire valid M-SEARCH message including initial method line,
+/// as per OCF specification (2020) section 1.3.2
+///
+/// #### Note:
+/// I've rarely actually seen a well-formed spec-conform M-SEARCH flying around my network
+/// but there's nothing wrong with actually being fully valid!
+impl Display for MSearch {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self {
+            host,
+            mx,
+            user_agent,
+            friendly_name,
+            uuid,
+        } = self;
+        writeln!(f, "{}", Method::MSearch)?;
+        host.write_header(f)?;
+        Man::Discover.write_header(f)?;
+        mx.write_header(f)?;
+        ST::All.write_header(f)?;
+        user_agent.write_header(f)?;
+        friendly_name.write_header(f)?;
+        uuid.write_header(f)?;
+        // Must end with blank line as per spec:
+        //   "Note: No body is present in requests with method M-SEARCH, but note that the
+        //          message shall have a blank line following the last header field."
+        writeln!(f)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+pub enum Host {
+    V4(SocketAddrV4),
+    /// IPv6 is currently untested & largely unimplemented
+    _V6(SocketAddrV6),
+}
+
+impl Header for Host {
+    const HEADER_KEY: &'static str = "HOST";
+}
+
+impl Display for Host {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Host::V4(socket_addr_v4) => write!(f, "{socket_addr_v4}"),
+            Host::_V6(socket_addr_v6) => write!(f, "{socket_addr_v6}"),
+        }
+    }
+}
+
+impl From<SocketAddr> for Host {
+    fn from(addr: SocketAddr) -> Self {
+        match addr {
+            SocketAddr::V4(socket_addr_v4) => Self::V4(socket_addr_v4),
+            SocketAddr::V6(socket_addr_v6) => Self::_V6(socket_addr_v6),
+        }
+    }
+}
+
+impl FromStr for Host {
+    type Err = AddrParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let addr = SocketAddr::from_str(s)?;
+        Ok(addr.into())
+    }
+}
+
+impl Default for Host {
+    fn default() -> Self {
+        MULTICAST.into()
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord)]
+/// A valid MX value (0..=5) (see UPnP spec para 1.3.2)
+///
+/// - Construct via `TryFrom<u8>`
+/// - Desconstruct via `Into<u8>`
+/// - Invalid values will result in an `io::ErrorKind::InvalidInput`.
+pub struct Mx(u8);
+
+impl Header for Mx {
+    const HEADER_KEY: &'static str = "MX";
+}
+
+impl Display for Mx {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl TryFrom<u8> for Mx {
+    type Error = io::Error;
+
+    fn try_from(mx: u8) -> Result<Self, Self::Error> {
+        match mx {
+            0..=5 => Ok(Self(mx)),
+            _ => Err(io::Error::from(io::ErrorKind::InvalidInput)),
+        }
+    }
+}
+
+impl From<Mx> for u8 {
+    fn from(mx: Mx) -> Self {
+        mx.0
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+struct UserAgent {
+    os: String,
+    os_version: String,
+    product_name: String,
+    product_version: String,
+}
+
+impl Header for UserAgent {
+    const HEADER_KEY: &'static str = "USER-AGENT";
+}
+
+/// Formatted as per OCF specification (2020) section 1.3.2 for the `USER-AGENT` *value*,
+/// does NOT include the header key
+impl Display for UserAgent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let Self {
+            os,
+            os_version,
+            product_name,
+            product_version,
+        } = self;
+        write!(
+            f,
+            "{os}/{os_version} UPnP/2.0 {product_name}/{product_version}"
+        )
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+pub struct FriendlyName(String);
+
+impl Header for FriendlyName {
+    const HEADER_KEY: &'static str = "CPFN.UPNP.ORG";
+}
+
+impl Display for FriendlyName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl From<&str> for FriendlyName {
+    fn from(s: &str) -> Self {
+        Self(s.to_string())
+    }
+}
+
+impl Header for Uuid {
+    const HEADER_KEY: &'static str = "CPUUID.UPNP.ORG";
+}
+
+/// Marker trait for Upnp header fields, with details of the relevant key
+trait Header {
+    /// Key as per spec
+    const HEADER_KEY: &'static str;
+}
+
+/// Handles constructing valid header lines.
+///
+/// This is a separate trait from [Header] to allow for it to also be implemented on `Option<H>`
+trait HeaderExt {
+    /// Generate a valid header line
+    fn to_header(&self) -> String;
+
+    /// Write a valid header line to `f` including new-line
+    fn write_header(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result;
+}
+
+impl<H: Header + Display> HeaderExt for H {
+    fn to_header(&self) -> String {
+        format!("{}: {}", Self::HEADER_KEY, self)
+    }
+
+    fn write_header(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", self.to_header())
+    }
+}
+
+impl<H: Header + HeaderExt> HeaderExt for Option<H> {
+    /// Generate a valid header line
+    ///
+    /// #### Note
+    /// - This will output an empty `String` for `None`.
+    ///   If this is not what you want consider using `.map(|h| h.to_header())` which
+    ///   will give you an `Option<String>` instead.
+    fn to_header(&self) -> String {
+        match self {
+            Some(header) => header.to_header(),
+            None => String::new(),
+        }
+    }
+
+    /// Write a valid header line to `f` including new-line
+    ///
+    /// #### Note
+    /// - `None` entries are handled nicely (no-op) *without* generating a blank line
+    fn write_header(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Some(header) => header.write_header(f),
+            None => Ok(()),
+        }
+    }
+}
 #[derive(Debug, Clone, PartialEq)]
 pub struct Notification {
     location: Option<String>,
@@ -284,6 +670,31 @@ CPUUID.UPNP.ORG: 2fac1234-31f8-11b4-a222-08002b34c003
             friendly_name,
             uuid,
         );
+        let msg_text = msg.to_string();
+        assert_eq!(msg_text, expected);
+    }
+
+    #[test]
+    fn search_no_user_agent() {
+        let expected = r#"M-SEARCH * HTTP/1.1
+HOST: 239.255.255.250:1900
+MAN: "ssdp:discover"
+MX: 5
+ST: ssdp:all
+CPFN.UPNP.ORG: splurt SSDP repeater
+CPUUID.UPNP.ORG: 2fac1234-31f8-11b4-a222-08002b34c003
+
+"#;
+        let mx = 5.try_into().expect("valid mx");
+        let friendly_name = "splurt SSDP repeater";
+        let uuid = uuid!("2fac1234-31f8-11b4-a222-08002b34c003");
+        let msg = MSearch {
+            host: Default::default(),
+            mx,
+            user_agent: None,
+            friendly_name: FriendlyName(friendly_name.to_string()),
+            uuid: Some(uuid),
+        };
         let msg_text = msg.to_string();
         assert_eq!(msg_text, expected);
     }
