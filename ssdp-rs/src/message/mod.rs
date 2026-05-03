d8a85676e4c5cf5a7f2a6e5a2e1ff0edba408ad5
Mike Foster <i.am.mike.foster@gmail.com>
2026-05-03 14:26:05 +0000

    update expected todo msg in test

--- ssdp-rs/src/message/mod.rs
+++ ssdp-rs/src/message/mod.rs
@@ -337,7 +337,7 @@ USN: uuid:2f402f80-da50-11e1-9b23-ecb55af4fe12e2c4::upnp:rootdevice
     }
 
     #[test]
-    #[should_panic(expected = "not yet implemented: urn:service_details")]
+    #[should_panic(expected = "not yet implemented: device or service")]
     fn parse_service() {
         let raw_response = r#"HTTP/1.1 200 OK
 CACHE-CONTROL: max-age=1900
