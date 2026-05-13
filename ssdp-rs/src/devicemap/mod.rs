e388a1f2e25111742d0d612bba9fe69e1b50414a
Mike Foster <i.am.mike.foster@gmail.com>
2026-05-13 14:05:12 +0000

    tidy response parsing

--- ssdp-rs/src/devicemap/mod.rs
+++ ssdp-rs/src/devicemap/mod.rs
@@ -85,86 +85,46 @@ impl From<Message> for Information {
                 port,
                 secure_location,
                 ..
-            }) => match usn.ntst {
-                ST::RootDevice => Self::Device {
-                    root_device: RootDevice::new(
-                        Some(usn.uuid),
-                        max_age,
-                        date,
-                        location,
-                        server,
-                        boot_id,
-                        config_id,
-                        port,
-                        secure_location,
-                    ),
-                    id: usn.uuid,
-                },
-                ST::Device(device) => {
-                    let id = usn.uuid;
-                    let embedded_device = EmbeddedDevice {
-                        id,
-                        device_type: Some(device),
-                        services: Default::default(),
-                    };
-                    let mut root_device = RootDevice::new(
-                        None,
-                        max_age,
-                        date,
-                        location,
-                        server,
-                        boot_id,
-                        config_id,
-                        port,
-                        secure_location,
-                    );
-                    root_device.embedded_devices.insert(id, embedded_device);
-                    Self::Device { root_device, id }
+            }) => {
+                let id = usn.uuid;
+                let mut embedded_device = EmbeddedDevice {
+                    id,
+                    device_type: None,
+                    services: Default::default(),
+                };
+                let mut root_device = RootDevice::new(
+                    None,
+                    max_age,
+                    date,
+                    location,
+                    server,
+                    boot_id,
+                    config_id,
+                    port,
+                    secure_location,
+                );
+                match usn.ntst {
+                    ST::RootDevice => {
+                        root_device.id = Some(usn.uuid);
+                        Self::Device { root_device, id }
+                    }
+                    ST::Device(device) => {
+                        embedded_device.device_type = Some(device);
+                        root_device.embedded_devices.insert(id, embedded_device);
+                        Self::Device { root_device, id }
+                    }
+                    ST::Service(service) => {
+                        embedded_device.services.insert(service);
+                        root_device.embedded_devices.insert(id, embedded_device);
+                        Self::Device { root_device, id }
+                    }
+                    ST::Uuid(id) => {
+                        root_device.embedded_devices.insert(id, embedded_device);
+                        Self::Device { root_device, id }
+                    }
+                    _ => todo!("other response"),
                 }
-                ST::Service(service) => {
-                    let id = usn.uuid;
-                    let mut embedded_device = EmbeddedDevice {
-                        id,
-                        device_type: None,
-                        services: Default::default(),
-                    };
-                    embedded_device.services.insert(service);
-                    let mut root_device = RootDevice::new(
-                        None,
-                        max_age,
-                        date,
-                        location,
-                        server,
-                        boot_id,
-                        config_id,
-                        port,
-                        secure_location,
-                    );
-                    root_device.embedded_devices.insert(id, embedded_device);
-                    Self::Device { root_device, id }
-                }
-                ST::Uuid(id) => {
-                    let embedded_device = EmbeddedDevice {
-                        id,
-                        device_type: None,
-                        services: Default::default(),
-                    };
-                    let mut root_device = RootDevice::new(
-                        None,
-                        max_age,
-                        date,
-                        location,
-                        server,
-                        boot_id,
-                        config_id,
-                        port,
-                        secure_location,
-                    );
-                    root_device.embedded_devices.insert(id, embedded_device);
-                    Self::Device { root_device, id }
-                }
-                _ => todo!("other response"),
-            },
+            }
             _ => todo!("other stuff"),
         }
     }
