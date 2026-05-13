0f46a4f5b97b6057e1f4c1e53413d5aa12c89e56
Mike Foster <i.am.mike.foster@gmail.com>
2026-05-13 14:02:38 +0000

    tidy Alive parsing

--- ssdp-rs/src/devicemap/mod.rs
+++ ssdp-rs/src/devicemap/mod.rs
@@ -37,66 +37,42 @@ impl From<Message> for Information {
                 config_id,
                 port,
                 secure_location,
-            })) => match usn.ntst {
-                NT::RootDevice => Self::Device {
-                    root_device: RootDevice::new(
-                        Some(usn.uuid),
-                        max_age,
-                        None,
-                        location,
-                        server,
-                        boot_id,
-                        config_id,
-                        port,
-                        secure_location,
-                    ),
-                    id: usn.uuid,
-                },
-                NT::Device(device) => {
-                    let id = usn.uuid;
-                    let embedded_device = EmbeddedDevice {
-                        id,
-                        device_type: Some(device),
-                        services: Default::default(),
-                    };
-                    let mut root_device = RootDevice::new(
-                        None,
-                        max_age,
-                        None,
-                        location,
-                        server,
-                        boot_id,
-                        config_id,
-                        port,
-                        secure_location,
-                    );
-                    root_device.embedded_devices.insert(id, embedded_device);
-                    Self::Device { root_device, id }
+            })) => {
+                let id = usn.uuid;
+                let mut embedded_device = EmbeddedDevice {
+                    id,
+                    device_type: None,
+                    services: Default::default(),
+                };
+                let mut root_device = RootDevice::new(
+                    None,
+                    max_age,
+                    None,
+                    location,
+                    server,
+                    boot_id,
+                    config_id,
+                    port,
+                    secure_location,
+                );
+                match usn.ntst {
+                    NT::RootDevice => {
+                        root_device.id = Some(id);
+                        Self::Device { root_device, id }
+                    }
+                    NT::Device(device) => {
+                        embedded_device.device_type = Some(device);
+                        root_device.embedded_devices.insert(id, embedded_device);
+                        Self::Device { root_device, id }
+                    }
+                    NT::Service(service) => {
+                        embedded_device.services.insert(service);
+                        root_device.embedded_devices.insert(id, embedded_device);
+                        Self::Device { root_device, id }
+                    }
+                    _ => todo!("other alive"),
                 }
-                NT::Service(service) => {
-                    let id = usn.uuid;
-                    let mut embedded_device = EmbeddedDevice {
-                        id,
-                        device_type: None,
-                        services: Default::default(),
-                    };
-                    embedded_device.services.insert(service);
-                    let mut root_device = RootDevice::new(
-                        None,
-                        max_age,
-                        None,
-                        location,
-                        server,
-                        boot_id,
-                        config_id,
-                        port,
-                        secure_location,
-                    );
-                    root_device.embedded_devices.insert(id, embedded_device);
-                    Self::Device { root_device, id }
-                }
-                _ => todo!("other alive"),
-            },
+            }
             Message::Search(_) => Self::ControlPoint(msg),
             Message::Response(Response {
                 max_age,
