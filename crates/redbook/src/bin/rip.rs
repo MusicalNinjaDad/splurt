561125d0343bbab31ef78d0aadb717c296c14ef3
Mike Foster <i.am.mike.foster@gmail.com>
2026-08-05 09:43:12 +0000

    try running directly as administrator via r-click from explorer. No difference for C: or E:

--- crates/redbook/src/bin/rip.rs
+++ crates/redbook/src/bin/rip.rs
@@ -3,9 +3,18 @@ use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let drive = &args[1];
+    let default_drive = "E:".to_string();
+    let drive = args.get(1).unwrap_or(&default_drive);
     if let Err(e) = read_toc(drive) {
         eprintln!("Error: {}", e);
+
+        println!("Press Enter to exit...");
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).ok();
+
         std::process::exit(1);
     }
+    println!("All OK - Press Enter to exit...");
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).ok();
 }
--- crates/redbook/src/lib.rs
+++ crates/redbook/src/lib.rs
@@ -9,11 +9,14 @@ use cd_da_reader::CdReader;
 
 pub fn read_toc(drive: &str) -> io::Result<()> {
     dbg!(&drive);
-    let drive = CdReader::open(&drive).unwrap();
-// [crates/redbook/src/lib.rs:11:5] &drive = "C:"
-// Device NOT opened succesfully
+    match CdReader::open(drive) {
+        Ok(_) => Ok(()),
+        Err(error) => Err(io::Error::other(error)),
+    }
 
-// thread 'main' (38736) panicked at crates/redbook/src/lib.rs:12:40:
-// called `Result::unwrap()` on an `Err` value: Os { code: 5, kind: PermissionDenied, message: "Access is denied." }
-    Ok(())
+    // [crates/redbook/src/lib.rs:11:5] &drive = "C:"
+    // Device NOT opened succesfully
+
+    // thread 'main' (38736) panicked at crates/redbook/src/lib.rs:12:40:
+    // called `Result::unwrap()` on an `Err` value: Os { code: 5, kind: PermissionDenied, message: "Access is denied." }
 }
