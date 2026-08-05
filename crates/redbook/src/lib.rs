fcebfcedee88c6086bbe7e6f44907e3661664638
Mike Foster <i.am.mike.foster@gmail.com>
2026-08-05 08:51:13 +0000

    more debug info

--- crates/redbook/src/lib.rs
+++ crates/redbook/src/lib.rs
@@ -1,15 +1,24 @@
 //! CDDA CD digital audio as per RedBook (IEC 60908:1999)
 
 use std::{
-    fs::File,
+    fs::OpenOptions,
     io::{self, Read},
 };
 
 pub fn read_toc(drive: &str) -> io::Result<()> {
-    let mut drive = File::open(drive)?;
-    const BYTES_TO_READ: usize = 2048;
+    dbg!(&drive);
+    let drive = OpenOptions::new().read(true).create(false).open(drive);
+    dbg!(&drive);
+// [crates/redbook/src/lib.rs:11:5] &drive = Err(
+//     Os {
+//         code: 5,
+//         kind: PermissionDenied,
+//         message: "Access is denied.",
+//     },
+// )
+    const BYTES_TO_READ: usize = 32;
     let mut toc = [0u8; BYTES_TO_READ];
-    let bytes_read = drive.read(&mut toc)?;
+    let bytes_read = drive?.read(&mut toc)?;
     dbg!(bytes_read);
     dbg!(toc);
     Ok(())
