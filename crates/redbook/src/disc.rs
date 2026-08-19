8d58ba68d2b712a4a5159cbfaf19905c98f68ef5
Mike Foster <i.am.mike.foster@gmail.com>
2026-08-19 14:03:43 +0000

    impl Disc

--- crates/redbook/src/disc.rs
+++ crates/redbook/src/disc.rs
@@ -1,7 +1,12 @@
+use std::io;
+
 use bytes::Bytes;
 use cdtoc::Toc;
 
-use crate::{Frame, Msf, Track, musicbrainz::DiscId};
+use crate::{
+    Frame, Msf, Track,
+    musicbrainz::{DiscId, Release},
+};
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 /// Logically: a physical CD.
@@ -45,21 +50,27 @@ impl Disc {
         leadout: Frame,
     ) -> Result<Self, DiscError> {
         let tracks: Vec<_> = tracks.into_iter().collect();
-        
-        (toc.leadout() == leadout.as_usize() as u32).ok_or(DiscError::IncorrectLeadout)?;
-        
+
+        if toc.leadout() != leadout.as_usize() as u32 {
+            return Err(DiscError::IncorrectLeadout);
+        }
+
         for track in tracks.iter() {
             let track_number = track.toc_entry.track as usize;
             let toc_track = toc
                 .audio_track(track_number)
                 .ok_or(DiscError::TocMismatch)?;
+
             let (min, sec, frame) = toc_track.msf();
-            (Msf::new(min as i8, sec as i8, frame as i8) == Msf::from(track.toc_entry.start))
-                .ok_or(DiscError::TocMismatch)?;
+            if Msf::new(min as i8, sec as i8, frame as i8) != Msf::from(track.toc_entry.start) {
+                return Err(DiscError::TocMismatch);
+            }
+
             let (d, h, min, sec, frame) = toc_track.duration().dhmsf();
             let min = (((d * 24) + h as u64) * 60) + min as u64;
-            (Msf::new(min as i8, sec as i8, frame as i8) == Msf::from(track.duration_frames))
-                .ok_or(DiscError::TocMismatch)?;
+            if Msf::new(min as i8, sec as i8, frame as i8) != Msf::from(track.duration_frames) {
+                return Err(DiscError::TocMismatch);
+            }
         }
 
         Ok(Self {
@@ -71,5 +82,92 @@ impl Disc {
             disc_index: None,
             coverart: None,
         })
+    }
+
+    /// Get the selected release
+    pub fn release(&self) -> Option<&Release> {
+        self.musicbrainz.as_ref()?.releases.get(self.release_index?)
+    }
+
+    /// Use the release at the given index, or reset selection to None.
+    /// Providing an invalid index will make no change.
+    ///
+    /// Returns a reference to the release set, to allow for validation.
+    pub fn set_release(&mut self, index: Option<usize>) -> &mut Self {
+        self.release_index = match index {
+            Some(index)
+                if self
+                    .musicbrainz
+                    .as_ref()
+                    .and_then(|disc_id| disc_id.releases.get(index))
+                    .is_some() =>
+            {
+                Some(index)
+            }
+            _ => None,
+        };
+        self
+    }
+
+    /// Provides an iterator over the releases to allow for programatic selection.
+    ///
+    /// Takes a closure which accepts a slice of releases and returns the index of
+    /// the release to select.
+    pub fn select_release<F>(&mut self, selector: F) -> &mut Self
+    where
+        F: FnOnce(&[Release]) -> Option<usize>,
+    {
+        if let Some(releases) = self.musicbrainz.as_ref().map(|disc_id| &disc_id.releases) {
+            self.set_release(selector(releases));
+        }
+        self
+    }
+
+    /// Attempt to update the data from musicbrainz
+    pub fn update_musicbrainz(&mut self) -> io::Result<&mut Self> {
+        let discid = self.toc.musicbrainz_id().to_string();
+        let url = format!("https://musicbrainz.org/ws/2/discid/{discid}?inc=recordings&fmt=json");
+
+        let client = reqwest::blocking::Client::new();
+        self.musicbrainz = Some(
+            client
+                .get(url)
+                .header("User-Agent", "splurt/0.1.0")
+                .send()
+                .map_err(io::Error::other)?
+                .json::<DiscId>()
+                .map_err(io::Error::other)?,
+        );
+        self.release_index = match self.musicbrainz {
+            None => Some(0),
+            Some(ref mb_data) if mb_data.releases.is_empty() => Some(0),
+            Some(ref mb_data) if mb_data.releases.len() == 1 => Some(1),
+            _ => None,
+        };
+        Ok(self)
+    }
+
+    /// Attempt to get the front cover art from the CoverArtArchive.
+    pub fn update_cover_art(&mut self) -> io::Result<&mut Self> {
+        let release_mbid = self
+            .release()
+            .ok_or_else(|| io::Error::other("No releases found"))?
+            .id
+            .clone();
+
+        let client = reqwest::blocking::Client::new();
+        let url = format!("https://coverartarchive.org/release/{release_mbid}/front");
+        let response = client
+            .get(url)
+            .header("User-Agent", "splurt/0.1.0")
+            .send()
+            .map_err(io::Error::other)?;
+        if response.status().is_success() {
+            let image = response.bytes().map_err(io::Error::other)?;
+            self.coverart = Some(image);
+        } else {
+            dbg!(response);
+        }
+        Ok(self)
     }
 }
